b0ce728c6b8af22b9fe4f535cfadd2c0eecdf8bc
Zack Graas <zcgraas@gmail.com>
2021-04-08 01:20:31 -0400

    env file creation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import Heading from './Components/Heading.js';
 import Collections from './Collections.js'
 
-
+console.log(process.env.REACT_APP_TMDB_API_KEY)
 
 function App() {
   return (
--- src/tmdbAPI.js
+++ src/tmdbAPI.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import firebase from 'firebase/app';
 import genre from './Components/GenreList.js';
-import key from './api';
+ 
 
-let apiKey = key;
+
 
 
 export function movieGet(){
     const db = firebase.firestore();
     const keyArr = Object.keys(genre);
     const valArr = Object.values(genre);
-
+    
     for(let i = 0, l = keyArr.length; i < l; i++){
-        axios.get('https://api.themoviedb.org/3/discover/movie?api_key='+ {apiKey} +'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1995-01-01&primary_release_date.lte=2020-10-01&vote_count.gte=1500&with_genres=' + valArr[i])
+        axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=1995-01-01&primary_release_date.lte=2020-10-01&vote_count.gte=1500&with_genres=` + valArr[i])
     .then (response => {
         db.collection('movie').doc(keyArr[i]).set(response.data);
         console.log('Success:'+keyArr[i])
@@ -22,7 +22,7 @@ export function movieGet(){
     });
     }
 
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key='+ {apiKey} +'&language=en-US&page=1')
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`)
     .then (response => {
         db.collection('movie').doc('popular').set(response.data);
         console.log('Success: Popular')
@@ -31,7 +31,7 @@ export function movieGet(){
         console.log(error);
     });
 
-    axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key='+ {apiKey} +'&language=en-US&page=1')
+    axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`)
     .then (response => {
         db.collection('movie').doc('upcoming').set(response.data);
         console.log('Success: Upcoming')
@@ -40,7 +40,7 @@ export function movieGet(){
         console.log(error);
     });
 
-    axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key='+ {apiKey})
+    axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_TMDB_API_KEY}`)
     .then( response=> {
         db.collection('movie').doc('trending').set(response.data);
         console.log('Success: Trending')
